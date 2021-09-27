7dfc734caea3b1e90d08ca626fd650419e9b3f54
Maor Magori <maor@magori.online>
2021-09-27 20:48:09 +0300

    Added simple configuration file.

---
+++ backend/config/config.js
@@ -0,0 +1,25 @@
+const convict = require("convict");
+
+const config = convict({
+  env: {
+    doc: "The application environment.",
+    format: ["production", "development", "test"],
+    default: "development",
+    env: "NODE_ENV",
+  },
+  ip: {
+    doc: "The IP address to bind.",
+    format: "ipaddress",
+    default: "127.0.0.1",
+    env: "IP_ADDRESS",
+  },
+  port: {
+    doc: "The port to bind.",
+    format: "port",
+    default: 3001,
+    env: "PORT",
+    arg: "port",
+  },
+});
+
+module.exports = config;
