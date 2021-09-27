02b85a0493699e68845b65e61a1cad1801a6f725
Maor Magori <maor@magori.online>
2021-09-28 01:44:01 +0300

    Added workers loading using convict.

---
+++ backend/workers/workers.js
@@ -0,0 +1,75 @@
+const convict = require("convict");
+const convict_format_with_validator = require("convict-format-with-validator");
+
+convict.addFormats(convict_format_with_validator);
+
+convict.addFormat({
+    name: "workers-array",
+    validate: (workers, scheme) => {
+        if (!Array.isArray(workers)) {
+            throw new Error("must be of type Array");
+        }
+
+        let ids = {};
+        for (worker of workers) {
+            if (ids[worker.store.id] === true)
+                throw new Error("Found duplicate workers");
+            else ids[worker.store.id] = true;
+
+            convict(scheme.children).load(worker).validate();
+        }
+    },
+});
+
+const workersScheme = {
+    workers: {
+        doc: "Collection of store scrapers",
+        format: "workers-array",
+        default: [],
+
+        children: {
+            name: {
+                doc: "worker's name",
+                format: String,
+                default: "",
+            },
+            ip: {
+                doc: "The worker ip",
+                format: "ipaddress",
+                default: "",
+            },
+
+            port: {
+                doc: "The port to the worker.",
+                format: "port",
+                default: "",
+            },
+            active: {
+                format: "Boolean",
+                default: true,
+            },
+            store: {
+                id: {
+                    doc: "The bookstore's id",
+                    format: String,
+                    default: "",
+                },
+                name: {
+                    doc: "The bookstore name",
+                    format: String,
+                    default: "",
+                },
+                base_url: {
+                    format: "url",
+                    default: "",
+                },
+            },
+        },
+    },
+};
+
+const workers = convict(workersScheme);
+workers.loadFile("./workers.json");
+workers.validate();
+
+module.exports = workers;
