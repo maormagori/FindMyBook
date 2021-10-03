b16d6f410a9eb8b262e9af03656dcdd322d7f8a4
Maor Magori <maor@magori.online>
2021-10-03 22:53:12 +0300

    Added more types to toolkit

--- common/toolkit/src/types.ts
+++ common/toolkit/src/types.ts
@@ -1,14 +1,41 @@
 type store = {
-    id: string|number,
-    name: string,
-    base_url: string
+    id: string | number;
+    name: string;
+    base_url: string;
 };
 
 export type manifest = {
-    name: string,
-    ip?: string,
-    port?: number,
-    active: boolean,
-    url: string,
-    store: store
-}
+    name: string;
+    ip?: string;
+    port?: number;
+    active: boolean;
+    url: string;
+    store: store;
+};
+
+type path = {
+    storeId: string | number;
+    name: string;
+    url: string;
+};
+
+type edition = {
+    print: "paperbook" | "digital" | "secondhand";
+    price: number;
+};
+
+export type book = {
+    name: string;
+    author: string;
+    url_paths: path[];
+    md5: string;
+    poster?: string;
+    description?: string;
+    publishing?: string;
+    translator?: string;
+    publishing_date?: string;
+    pages?: number;
+    isbn?: string;
+    danacode?: string;
+    editions: edition[];
+};
